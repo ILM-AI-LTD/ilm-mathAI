be5be1a2f1311d9b413a1fcc4279ee0d5e44cf26
aspakdevops <106157682+aspakdevops@users.noreply.github.com>
2025-09-04 20:05:45 +0530

    Update notify-slack.sh

--- scripts/notify-slack.sh
+++ scripts/notify-slack.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-echo "Waiting 200 seconds to allow Elastic Beanstalk deployment to settle..."
+echo "Waiting 120 seconds to allow Elastic Beanstalk deployment to settle..."
 sleep 120
 
 # Get current branch name
 BRANCH_NAME=${GITHUB_HEAD_REF:-${GITHUB_REF##*/}}
 
 # Fetch Beanstalk environment CNAME (URL)
-APP_URL=https://ilmath.ilmino.com
+APP_URL=https://platform.math.ilmino.com
 
 # Get Elastic Beanstalk health
 HEALTH=$(aws elasticbeanstalk describe-environments \
@@ -23,8 +23,14 @@ HTTP=$(curl -s -o /dev/null -w "%{http_code}" "http://ilm-math-ai.eu-west-2.elas
 # Get deployer info
 DEPLOYER=${GITHUB_TRIGGERING_ACTOR:-$GITHUB_ACTOR}
 
+# Get repo URL
+REPO_URL="https://github.com/${GITHUB_REPOSITORY}/"
+
+# Branch URL
+BRANCH_URL="${REPO_URL}tree/${BRANCH_NAME}"
+
 # Send Slack message
-MESSAGE="*CI/CD Pipeline Status*: $1\n*Branch*: $BRANCH_NAME\n*Deployed by*: $DEPLOYER\n*Elastic Beanstalk Health*: $HEALTH\n*HTTP Status*: $HTTP\n*App*: $EB_APP_NAME\n*Env*: $EB_ENV_NAME\n*URL*: $APP_URL"
+MESSAGE="*CI/CD Pipeline Status*: $1\n*Branch*: <${BRANCH_URL}|${BRANCH_NAME}>\n*Deployed by*: $DEPLOYER\n*Elastic Beanstalk Health*: $HEALTH\n*HTTP Status*: $HTTP\n*App*: $EB_APP_NAME\n*Env*: $EB_ENV_NAME\n*URL*: $APP_URL\n*Repo*: $REPO_URL"
 
 curl -X POST -H 'Content-type: application/json' \
      --data "{\"text\":\"$MESSAGE\"}" "$SLACK_WEBHOOK_URL" || echo "Slack notification failed, continuing..."
